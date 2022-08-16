3d683d65aea09914b65d2c88bdd526c8e76770b4
Vladislav izmailov <arielmap@icloud.com>
2022-08-17 02:34:18 +0300

    task23

---
+++ task_23/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число ");
+double number1 = Convert.ToDouble(Console.ReadLine());
+double number2 = 1;
+while (number2 <= number1)
+{
+    Console.WriteLine(Math.Pow(number2, 3) + " ");
+    number2++;
+}
+Console.WriteLine(" ");
