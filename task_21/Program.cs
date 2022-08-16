6389a35377ed506520dd40e2c6eda57a19317c47
Vladislav izmailov <arielmap@icloud.com>
2022-08-17 02:28:30 +0300

    task21

---
+++ task_21/Program.cs
@@ -0,0 +1,18 @@
+﻿double[] array1 = new double[3];
+double[] array2 = new double[3];
+double sum = 0;
+Console.WriteLine("Введите первую точку ");
+for (int i = 0; i < 3; i++)
+{
+    array1[1] = Convert.ToDouble(Console.ReadLine());
+}
+Console.WriteLine("Введите вторую точку");
+for (int i = 0; i < 3; i++)
+{
+    array2[i] = Convert.ToDouble(Console.ReadLine());
+}
+for (int i = 0; i < 3; i++)
+{
+    sum += Math.Pow(array1[i] - array2[i], 2);
+}
+Console.WriteLine(Math.Sqrt(sum));
