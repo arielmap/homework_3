c6cb4facc3cd4e368d3a8d7e7affa1ec76262a83
Vladislav izmailov <arielmap@icloud.com>
2022-08-17 02:16:09 +0300

    homework19

---
+++ task_19/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] array = new int[5];
+int number = Convert.ToInt32(Console.ReadLine());
+int flag = 0;
+int leng = array.Length;
+while (number > 0)
+{
+    array[leng - 1] = number % 10;
+    number /= 10;
+    leng--;
+}
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] != array[array.Length - i - 1])
+    {
+        flag = 1;
+    }
+}
+if (flag == 1)
+{
+    Console.WriteLine("Нет, это не палиндром.");
+}
+else
+{
+    Console.WriteLine("Да, это палиндром.");
+}
